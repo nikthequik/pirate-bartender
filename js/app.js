52cb4b1b2f89dc330bc8fcca7e83f47791898acb
nikthequik <quik.the.nick@gmail.com>
2016-05-29 10:08:38 -0600

    -add more constructors and start creating objects with them

--- js/app.js
+++ js/app.js
@@ -1,21 +1,33 @@
 $(function() {
-
+	
 	function Question(lead, subject) {
 		this.lead = lead;
 		this.subject = subject;
 	};
 
-	function Ingredient(strong, salty, bitter, sweet, fruity) {
-		this.strong = strong;
-		this.salty = salty;
-		this.bitter = bitter;
-		this.sweet = sweet;
-		this.fruity = fruity;
+	Question.prototype.createQuestion = function(lead, subject) {
+		var newQuestion = lead + subject + "?";
+	};
+
+	function Ingredient(name, flavor) {
+		this.name = name;
+		this.flavor = flavor;
 	};
 
 	function Pantry(ingredient, flavor) {
 		this.ingredient = ingredient;
 		this.flavor = flavor;
 	};
+
+	function Bartender() {};
+	Bartender.prototype.createDrink = function(userPref){
+
+	};
+
+	var moe = new Bartender();
+
+	var strongQ = new Question("Do you like it when your drinks are ", "strong");
+
+	var preference = {};
 
 });
