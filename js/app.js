55d6f23a929b9b2dc74c4b16659ae341643e51db
nikthequik <quik.the.nick@gmail.com>
2016-05-29 13:17:38 -0600

    -set up background of pirate bartender -add images

--- js/app.js
+++ js/app.js
@@ -20,14 +20,27 @@ $(function() {
 	};
 
 	function Bartender() {};
+	Bartender.prototype.questions = [];
 	Bartender.prototype.createDrink = function(userPref){
+
+	};
+
+	Bartender.prototype.askQuestion = function() {
 
 	};
 
 	var moe = new Bartender();
 
 	var strongQ = new Question("Do you like it when your drinks are ", "strong");
+	var saltyQ = new Question("Are you a fan of a drink that can be described as ", "salty");
+	var bitterQ = new Question("Are you in the mood for something ", "bitter");
+	var sweetQ = new Question("Would you like me to make your drink ", "sweet");
+	var fruityQ = new Question("Would it be alright if you could describe your drink as ", "fruity");
+	moe.questions.push(strongQ, saltyQ, bitterQ, sweetQ, fruityQ);
+	console.log(moe.questions[0].subject);
 
 	var preference = {};
+	
+
 
 });
