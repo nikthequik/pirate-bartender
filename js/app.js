1214edddcc1463c80b62b886b34252fde070be5e
nikthequik <quik.the.nick@gmail.com>
2016-05-29 23:19:03 -0600

    -add dialogue box -add askQuestion method to bartender -work on createDrink -add qCount, but need if statement for end of createDrink

--- js/app.js
+++ js/app.js
@@ -1,32 +1,49 @@
 $(function() {
 	
+	var qCount = 0;
+
 	function Question(lead, subject) {
 		this.lead = lead;
 		this.subject = subject;
 	};
 
-	Question.prototype.createQuestion = function(lead, subject) {
-		var newQuestion = lead + subject + "?";
-	};
-
-	function Ingredient(name, flavor) {
-		this.name = name;
+	function Ingredient(iName, flavor) {
+		this.iName = iName;
 		this.flavor = flavor;
 	};
 
-	function Pantry(ingredient, flavor) {
+	function Pantry(ingredient, type) {
 		this.ingredient = ingredient;
 		this.flavor = flavor;
 	};
 
+	function Drink() {};
+
 	function Bartender() {};
 	Bartender.prototype.questions = [];
 	Bartender.prototype.createDrink = function(userPref){
+		var newdrink = new Drink();
+		if (userPref.strong === true) {
 
+		}
+		if (userPref.salty === true) {
+			
+		}
+		if (userPref.bitter === true) {
+			
+		}
+		if (userPref.sweet === true) {
+			
+		}
+		if (userPref.fruity === true) {
+			
+		}
 	};
-
 	Bartender.prototype.askQuestion = function() {
-
+		var index = Math.floor((Math.random() * 5) + 1);
+		var newQuestion = this.questions[index].lead + this.questions[qCount].subject + "?";
+		$("#dialogue").html("<p class='question'>" + newQuestion + "</p><button class='input'>Yes</button><button class='input'>No</button>");
+		qCount ++;
 	};
 
 	var moe = new Bartender();
@@ -36,11 +53,17 @@ $(function() {
 	var bitterQ = new Question("Are you in the mood for something ", "bitter");
 	var sweetQ = new Question("Would you like me to make your drink ", "sweet");
 	var fruityQ = new Question("Would it be alright if you could describe your drink as ", "fruity");
+
 	moe.questions.push(strongQ, saltyQ, bitterQ, sweetQ, fruityQ);
-	console.log(moe.questions[0].subject);
 
-	var preference = {};
+	var preference = {
+		strong: "",
+		salty: "",
+		bitter: "",
+		sweet: "",
+		fruity: ""
+	};
 	
-
+	moe.askQuestion();
 
 });
